5394dc05d5e5e7ddc068d5677accadcd5d8b9ccf
horlartundhey <horlartundhey@gmail.com>
2021-12-30 09:16:30 +0000

    New updates for Uploads

---
+++ application/logs/log-2021-12-30.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-12-30 10:08:20 --> 404 Page Not Found: Images/resources
+ERROR - 2021-12-30 10:08:20 --> 404 Page Not Found: Images/resources
+ERROR - 2021-12-30 10:08:20 --> 404 Page Not Found: Images/resources
+ERROR - 2021-12-30 10:08:20 --> 404 Page Not Found: Images/logo2.png
+ERROR - 2021-12-30 10:08:20 --> 404 Page Not Found: Images/resources
+ERROR - 2021-12-30 10:08:20 --> 404 Page Not Found: Images/resources
+ERROR - 2021-12-30 10:08:20 --> 404 Page Not Found: Images/resources
+ERROR - 2021-12-30 10:08:25 --> 404 Page Not Found: Edit-interesthtml/index
